8c71feb598b5b2b98dbc4981ef98751b9c1535f1
Deniss <den.b@itservicesgroup.co.uk>
2013-10-24 14:54:06 +0100

    rspec passed product-model index updated (price and description)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should have a price' do
+  	product=Product.create(price: 230.99)
+  	expect(product.missing_details?).to be_true
+	end
+
+	it 'should have a description' do
+		product =Product.create(description:'something here')
+  	expect(product).to be_missing_details
+	end	
+
+	it 'should have a description OR price ' do
+		product =Product.create(description:'something here', price: 8)
+  	expect(product).not_to be_missing_details
+	end
 end
